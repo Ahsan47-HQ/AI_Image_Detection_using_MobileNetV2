9ae0905f895547d8b0142f9ceb01b13a47228c38
Ahsan <ahsan.bagsaria@gmail.com>
2026-03-30 19:00:25 +0000

    Integrated Tensorflow Lite

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 import numpy as np
 from PIL import Image
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 
 # Load TFLite model
 interpreter = tf.lite.Interpreter(model_path="model/model.tflite")
@@ -12,7 +12,7 @@ output_details = interpreter.get_output_details()
 def preprocess_image(image):
     image = image.resize((224, 224))
     image = np.array(image, dtype=np.float32)
-    image = tf.keras.applications.mobilenet_v2.preprocess_input(image)
+    image = (image / 127.5) - 1.0
     image = np.expand_dims(image, axis=0)
     return image
 
