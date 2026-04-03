6e1e32528276131658d613439546998a996a6864
Ahsan <ahsan.bagsaria@gmail.com>
2026-04-03 13:27:43 +0000

    Added backend files

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from predict import predict
+from PIL import Image
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/predict", methods=["POST"])
+def predict_api():
+    file = request.files["image"]
+    image = Image.open(file).convert("RGB")
+
+    label, confidence = predict(image)
+
+    return jsonify({
+        "label": label,
+        "confidence": confidence
+    })
+
+if __name__ == "__main__":
+    app.run(port=5000)
