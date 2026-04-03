e430a2cb62800a3b297e3d019850f1938014c026
Ahsan <ahsan.bagsaria@gmail.com>
2026-04-03 14:10:54 +0000

    fixed errors

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from backend.predict import predict
+from predict import predict
 from PIL import Image
 
 app = Flask(__name__)
